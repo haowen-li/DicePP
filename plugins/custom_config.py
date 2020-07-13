b55fb214b8b94db4eb5bb8c1b6bd4094cbe3cc89
Haowen <u6342101@anu.edu.au>
2020-07-13 11:16:05 +1000

    更新查询资料中的错误

--- plugins/custom_config.py
+++ plugins/custom_config.py
@@ -12,6 +12,7 @@ GROUP_PASSWORD = 'dnd5e-pear'
 # 环境信息
 IS_COOLQ_PRO = True
 PLATFORM_NAME = 'DOCKER' # 可选为 DOCKER 或 其他任意字符串
+# PLATFORM_NAME = 'WIN' # 可选为 DOCKER 或 其他任意字符串
 # 刷屏上限
 MESSAGE_LIMIT_NUM = 12
 # 刷屏阈值
@@ -29,6 +30,10 @@ DAILY_CREDIT_LIMIT = 15
 # 查询最多显示的条目
 QUERY_SHOW_LIMIT = 50
 
+WINE_COOLQ_PATH = 'Z:/home/user/coolq'
+WINE_COOLQ_JOKEIMG_PATH = WINE_COOLQ_PATH + '/data/image/joke/'
+WINE_COOLQ_EMOTIMG_PATH = WINE_COOLQ_PATH + '/data/image/emotion/'
+
 LOCAL_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 LOCAL_NICKNAME_PATH = os.path.join(LOCAL_DATA_PATH, 'nick_name.json')
 LOCAL_INITINFO_PATH = os.path.join(LOCAL_DATA_PATH, 'init_info.json')
@@ -42,11 +47,17 @@ LOCAL_CUSTOM_DATA_PATH = os.path.join(os.path.dirname(__file__), 'custom_data')
 LOCAL_QUERYINFO_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'query_info')
 LOCAL_DECKINFO_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'deck_info')
 LOCAL_MENUINFO_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'menu_info')
-LOCAL_JOKEIMG_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'joke_img')
-LOCAL_EMOTIMG_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'emotion_img')
+
 LOCAL_JOKEINFO_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'joke.json')
 LOCAL_NAMEINFO_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'name.json')
 LOCAL_QUESINFO_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'question.json')
+
+if PLATFORM_NAME == 'DOCKER':
+    LOCAL_JOKEIMG_DIR_PATH = WINE_COOLQ_JOKEIMG_PATH
+    LOCAL_EMOTIMG_DIR_PATH = WINE_COOLQ_EMOTIMG_PATH
+else:
+    LOCAL_JOKEIMG_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'joke_img')
+    LOCAL_EMOTIMG_DIR_PATH = os.path.join(LOCAL_CUSTOM_DATA_PATH, 'emotion_img')
 
 ALL_LOCAL_DATA_PATH = [LOCAL_NICKNAME_PATH, LOCAL_INITINFO_PATH, LOCAL_PCSTATE_PATH,
                        LOCAL_GROUPINFO_PATH, LOCAL_USERINFO_PATH, LOCAL_TEAMINFO_PATH,
@@ -55,10 +66,6 @@ ALL_LOCAL_DATA_PATH = [LOCAL_NICKNAME_PATH, LOCAL_INITINFO_PATH, LOCAL_PCSTATE_P
 ALL_LOCAL_DIR_PATH = [LOCAL_DATA_PATH, LOCAL_CUSTOM_DATA_PATH, LOCAL_QUERYINFO_DIR_PATH,
                       LOCAL_DECKINFO_DIR_PATH, LOCAL_DECKINFO_DIR_PATH, LOCAL_MENUINFO_DIR_PATH,
                       LOCAL_JOKEIMG_DIR_PATH, LOCAL_EMOTIMG_DIR_PATH]
-
-WINE_COOLQ_PATH = 'Z:/home/user/coolq'
-WINE_COOLQ_JOKEIMG_PATH = WINE_COOLQ_PATH + '/data/image/joke/'
-WINE_COOLQ_EMOTIMG_PATH = WINE_COOLQ_PATH + '/data/image/emotion/'
 
 for dirPath in ALL_LOCAL_DIR_PATH:
     if os.path.exists(dirPath) == False:
@@ -69,7 +76,3 @@ for path in ALL_LOCAL_DATA_PATH:
     if os.path.exists(path) == False:
         UpdateJson({}, path)
         print("Create file: " + path)
-
-if PLATFORM_NAME == 'DOCKER':
-    LOCAL_JOKEIMG_DIR_PATH = WINE_COOLQ_JOKEIMG_PATH
-    LOCAL_EMOTIMG_DIR_PATH = WINE_COOLQ_EMOTIMG_PATH
